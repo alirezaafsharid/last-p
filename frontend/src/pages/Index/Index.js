d9f4582d864e82b356a66aeae3a39e01dceeed3c
alireza <afshar2451382@gmail.com>
2024-09-20 19:15:25 +0330

    هدر و نوبار صفحه اصلی سایت

--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import Header from "../../Component/Header/Header";
 
-import './Index.css'
+import "./Index.css";
 
 export default function Index() {
-  return (
-    <div>Index</div>
-  )
+  return <Header />;
 }
