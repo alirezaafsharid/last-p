5211cb156e38bf4ccdb54162906139c1b408c635
alireza <afshar2451382@gmail.com>
2024-09-21 15:55:50 +0330

    توسعه صفحه اصلی پروزه تا قسکت درباره ما

--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "../../Component/Header/Header";
+import LastCourses from "../../Component/LastCourses/LastCourses";
 
 import "./Index.css";
+import AboutUs from "../../Component/AboutUs/AboutUs";
 
 export default function Index() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <LastCourses />
+      <AboutUs />
+    </>
+  );
 }
